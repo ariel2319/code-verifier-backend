35162e2567117ff57d96ff4c672ae3a98024d98e
areil <maldonadoaa.91@gmail.com>
2023-04-24 01:17:44 -0300

    server is starting

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,7 +7,7 @@
 
 import express, { Request, Response } from "express";
 import helloRouter from "./HelloRouter";
-import { LogSuccess } from "@/utils/logger";
+import { LogInfo } from "@/utils/logger";
 
 //Server Instance
 let app = express(); // -> nos permite elegir a qué dirección enviar las consultas
@@ -17,8 +17,17 @@ let rootRouter = express.Router();
 
 //Activate for resuests to http://localhost:8080/api
 //GET: http://localhost:8080/api (raíz)
-rootRouter.get('/', (req: Request, res: Response)=> {
+rootRouter.get('/', (req: Request, res: Response) => {
+  LogInfo('GET: // http://localhost:8080/api/')
+  //Send Hello World
   res.send('Welcome to my API Restful: Express + TS + Nodemon + Jest + Swagger + Mongoose');
 });
 
 //Redirections to Routers & Controllers
+//usamos la función USE
+app.use('/', rootRouter); // http://localhost:8080/api/
+app.use('/hello', helloRouter); // http://localhost:8080/api/hello  -> gestionadas por Herllo Router
+
+//add more routes NEXT
+
+export default app;
---
+++ src/server/index.js
@@ -0,0 +1,3 @@
+import express, { Express, Response, Request } from "express";
+import dotenv from 'dotenv';
+
